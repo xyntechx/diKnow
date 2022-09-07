cb9e3b2b809ae54980e6a20fca4ca202e30e083f
xyntechx <xyntechx@gmail.com>
2022-09-07 20:13:57 +0800

    Dynamic dino page

--- routes/[dino].tsx
+++ routes/[dino].tsx
@@ -1,7 +1,22 @@
 /** @jsx h */
 import { h } from "preact";
+import { tw } from "@twind";
 import { PageProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
+import Dino from "../islands/Dino.tsx";
 
-export default function Greet(props: PageProps) {
-  return <div>{props.params.name}</div>;
-}
+const DinoPage = (props: PageProps) => {
+  return (
+    <div>
+      <Head>
+        <title>diKnow | {props.params.dino}</title>
+        <meta name="description" content="Learn about dinosaurs" />
+      </Head>
+      <main class={tw`flex flex-col items-center justify-start my-10`}>
+        <Dino name={props.params.dino} />
+      </main>
+    </div>
+  );
+};
+
+export default DinoPage;
